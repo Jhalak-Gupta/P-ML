90bbb96bc97ec28117cc376029898c4d94e98603
jhalakgupta <jhalakgup@gmail.com>
2024-06-09 19:04:19 +0530

    Commit 2

--- Assignment 1.py
+++ Assignment 1.py
@@ -22,8 +22,17 @@ name=input("Enter your name:")
 print("Welcome",name)
 
 #Question5
+xy=input("Enter the string:")
+f1=open("data.txt","w")
+f1.write(xy)
+print("String added to the file")
+f1.close()
 
 #Question6
+f2=open("data.txt","r")
+for i in f2.read():
+    print(i)
+f2.close()
 
 #Question7
 str=input("Enter the string:")
@@ -74,8 +83,22 @@ by=int(input("Enter your birth year:"))
 print("Age=",py-by)
 
 #Question14
+lines=[]
+while True:
+    line=input("Enter text lines:")
+    if line!=" ":
+        lines.append(line)
+    else:
+        break
+for line in lines:
+    print(line)
 
 #Question15
+import csv
+f3=open("data.csv","r")
+for i in csv.reader(f3):
+    print(i)
+f3.close()
 
 #Question16
 string=input("Enter the string:")
@@ -93,8 +116,26 @@ st=input("Enter the string:")
 print(st.title())
 
 #Question18
+string1=input("Enter the first string: ")
+string2=input("Enter the second string: ")
+stringx1=sorted(string1)
+stringx2=sorted(string2)
+if len(stringx1)!=len(stringx2):
+    print("The strings are not anagrams of each other.")
+else:
+    if stringx1==stringx2:
+        print("The strings are anagrams of each other.")
+    else:
+        print("The strings are not anagrams of each other.")
 
 #Question19
+si = input("Enter the string:")
+p='''!()-[]{};:'",<>./?@#$%^&*_~'''
+sip = ""
+for i in si:
+    if i not in p:
+        sip = sip + i
+print(sip)
 
 #Question20
 l=eval(input("Enter the list of numbers:"))
@@ -121,6 +162,17 @@ print("Maximum Value=",max(lst))
 print("Minimum Value=",min(lst))
 
 #Question23
+conv=input("Enter '1' to convert from Celsius to Fahrenheit, or '2' to convert from Fahrenheit to Celsius: ")
+if conv=='1':
+    celsius = float(input("Enter temperature in Celsius: "))
+    fahrenheit = (celsius * 9/5) + 32
+    print("Temperature in Fahrenheit:", fahrenheit)
+elif conv=='2':
+    fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+    celsius = (fahrenheit - 32) * 5/9
+    print("Temperature in Celsius:", celsius)
+else:
+    print("Invalid input. Please enter '1' or '2' for conversion.")
 
 #Question24
 Oper=input("Enter the operator (+,-,*,/): ")
@@ -138,6 +190,11 @@ else:
     print("Invalid")
 
 #Question25
+source_file=open("data1.txt", "r")
+content=source_file.read()
+destination_file=open("data2.txt", "w")
+destination_file.write(content)
+print("File copied successfully")
 
 #Question26
 s1=input("Enter the string:")
